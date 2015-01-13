83aea9d87ec16789259eb9082aef756f9194f9cf
smallfatcat <david.imrie@gmail.com>
2015-01-13 12:18:31 +0000

    first change

--- TypeScriptHTMLApp1/app.js
+++ TypeScriptHTMLApp1/app.js
@@ -1,7 +1,7 @@
 ﻿var Greeter = (function () {
     function Greeter(element) {
         this.element = element;
-        this.element.innerHTML += "The time is: ";
+        this.element.innerHTML += "Hello there. The time is: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
--- TypeScriptHTMLApp1/app.ts
+++ TypeScriptHTMLApp1/app.ts
@@ -5,7 +5,7 @@
 
     constructor(element: HTMLElement) {
         this.element = element;
-        this.element.innerHTML += "The time is: ";
+        this.element.innerHTML += "Hello there. The time is: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
